bd699032c8d4bc7744a57d860d22bc37e8a0f807
Patrick Hall <patrickhall@billericak12.com>
2021-10-28 11:28:54 -0400

    woo

--- src/com/company/DualSpinner.java
+++ src/com/company/DualSpinner.java
@@ -14,23 +14,34 @@ public class DualSpinner {
         if (playerSpin > CPUSpin) {
             playerPoints += (playerSpin - CPUSpin);
             System.out.println("You Win! " + playerPoints + " points");
+            System.out.println(playerSpin);
+            System.out.println(CPUSpin);
         }
 
-        if (playerSpin < CPUSpin) {
+        else if (playerSpin < CPUSpin) {
             playerPoints -= (CPUSpin - playerSpin);
             System.out.println("You Lose. " + playerPoints + " points");
+            System.out.println(playerSpin);
+            System.out.println(CPUSpin);
         } else {
             int newPlayerSpin = spin(1, 10);
             int newCPUSpin = spin(2, 8);
             if (playerSpin + newPlayerSpin > CPUSpin + newCPUSpin) {
                 playerPoints += 1;
                 System.out.println("You Win 2! " + playerPoints + " points");
+                System.out.println(playerSpin + newPlayerSpin);
+                System.out.println(CPUSpin + newCPUSpin);
             }
-            if (playerSpin + newPlayerSpin < CPUSpin + newCPUSpin) {
+            else if (playerSpin + newPlayerSpin < CPUSpin + newCPUSpin) {
                 playerPoints -= 1;
                 System.out.println("You Lose 2. " + playerPoints + " points");
-            } else {
+                System.out.println(playerSpin + newPlayerSpin);
+                System.out.println(CPUSpin + newCPUSpin);
+            }
+            else {
                 System.out.println("Tie. " + playerPoints + " points");
+                System.out.println(playerSpin + newPlayerSpin);
+                System.out.println(CPUSpin + newCPUSpin);
             }
         }
     }
